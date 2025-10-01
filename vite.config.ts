df9d7d6a3ca743cb38c1c1e00db056b0e4cfb417
Domenik Reitzner <domenik.reitzner@woom.com>
2025-10-01 16:05:07 +0200

    ECOMM-3033 try with cloudflare vite plugin 🔌

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import build from "@hono/vite-build/cloudflare-pages";
 import devServer from "@hono/vite-dev-server";
 import adapter from "@hono/vite-dev-server/cloudflare";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     build(),
+    cloudflare(),
     devServer({
       adapter,
       entry: "src/index.ts",
