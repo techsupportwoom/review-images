3009a8aa1920113bdda30b0516aff1db4b14764a
Domenik Reitzner <domenik.reitzner@woom.com>
2025-10-01 16:16:00 +0200

    ECOMM-3033 remove cloudflare vite plugin 🔌

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import build from "@hono/vite-build/cloudflare-pages";
 import devServer from "@hono/vite-dev-server";
 import adapter from "@hono/vite-dev-server/cloudflare";
-import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     build(),
-    cloudflare(),
     devServer({
       adapter,
       entry: "src/index.ts",
